4b03a54366a56525ddaf589e2466c85c9a87b80e
Nadia Makeeva <lossa@inbox.ru>
2023-04-24 21:43:09 +0300

    change 1 file

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Cli {
     public static void getUserName() {
+        System.out.println("Welcome to the Brain Games!");
         Scanner scan = new Scanner(System.in);
         System.out.println("May I have your name? ");
         String name = scan.nextLine();
